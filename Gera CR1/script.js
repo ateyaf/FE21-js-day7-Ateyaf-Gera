c8347c54d2fc3cbabb83885fc9e7b7d7748803da
Gergely Izsak <izsak.gera@gmail.com>
2021-07-05 10:46:17 +0200

    finished

---
+++ Gera CR1/script.js
@@ -0,0 +1,27 @@
+let timerInterval
+Swal.fire({
+    title: 'Auto close alert!',
+    html: 'I will close in <b></b> milliseconds.',
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading()
+        timerInterval = setInterval(() => {
+            const content = Swal.getHtmlContainer()
+            if (content) {
+                const b = content.querySelector('b')
+                if (b) {
+                    b.textContent = Swal.getTimerLeft()
+                }
+            }
+        }, 100)
+    },
+    willClose: () => {
+        clearInterval(timerInterval)
+    }
+}).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+        console.log('I was closed by the timer')
+    }
+})
